3d5e8e05e671e177536b5ce2bf35b312141e7f0b
MarkyMan4 <markus.hutnik@gmail.com>
2021-05-23 16:10:32 -0500

    get the page number and total number of pages from response

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,14 +5,20 @@ import ArticleCard from '../components/articleCard';
 
 function Home() {
     const [articles, setArticles] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1); // initialize to 1 so it starts at first page
+    const [totalPages, setTotalPages] = useState(0);
 
     useEffect(() => {
-        getArticlePage(1).then(res => setArticles(res));
+        getArticlePage(currentPage).then(res => {
+            setArticles(res.articles);
+            setTotalPages(res.total_pages);
+        });
     }, []);
 
     return (
         <div>
             <h1 className="text-center animate__animated animate__flipInX">Recent Articles</h1>
+            <div className="text-center">Page {currentPage} of {totalPages}</div>
             {articles.map((art, indx) => {
                 return <ArticleCard key={indx} article={art} />;
             })}
