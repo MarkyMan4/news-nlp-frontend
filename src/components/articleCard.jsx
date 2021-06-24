b2d90513b072a3c51ca4be4846ab4a45a5efad04
MarkyMan4 <markus.hutnik@gmail.com>
2021-06-23 21:53:21 -0500

    show topic name on article card

--- src/components/articleCard.jsx
+++ src/components/articleCard.jsx
@@ -7,9 +7,16 @@ function ArticleCard(props) {
             <Link to={"/article/" + props.article.id}><h4>{props.article.headline}</h4></Link>
             <div><b>Keywords: </b>{props.article.nlp.keywords}</div>
             <hr />
-            <div><b>Date Published:</b> {props.article.date_published}</div>
-            <div><b>Publisher: </b>{props.article.publisher}</div>
-            <div><b>Sentiment: </b>{props.article.nlp.sentiment} -- <b>Subjectivity: </b>{props.article.nlp.subjectivity}</div>
+            <div className="row">
+                <div className="col-md-6">
+                    <div><b>Date Published:</b> {props.article.date_published}</div>
+                    <div><b>Publisher: </b>{props.article.publisher}</div>
+                    <div><b>Sentiment: </b>{props.article.nlp.sentiment} -- <b>Subjectivity: </b>{props.article.nlp.subjectivity}</div>
+                </div>
+                <div className="col-md-6 text-right">
+                    {props.article.nlp.topic_name}
+                </div>
+            </div>
         </div>
     )
 }
