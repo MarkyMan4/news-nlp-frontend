bfaf58cc0277e00fa21b48c2c7d8e5eb75d80ed3
MarkyMan4 <markus.hutnik@gmail.com>
2021-06-23 22:25:55 -0500

    do not let unregistered users view saved article page

--- src/pages/savedArticles.jsx
+++ src/pages/savedArticles.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { isUserAuthenticated } from '../utils/storage';
+// import { Redirect } from 'react-router-dom';
 
 function SavedArticles() {
-    return (
-        <div>
-            <h1 className="text-center animate__animated animate__flipInX">Saved Articles</h1>
-        </div>
-    )
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        isUserAuthenticated()
+            .then(res => {
+                console.log(`in useeffect: ${isLoggedIn}`)
+                setIsLoggedIn(res)
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+    const getPageContent = () => {
+        let content;
+
+        console.log(`in method: ${isLoggedIn}`);
+
+        if(isLoggedIn) {
+            content = (
+                <div>
+                    <h1 className="text-center animate__animated animate__flipInX">Saved Articles</h1>
+                </div>
+            );
+        }
+        else {
+            content = <h1>you must be logged in to save articles</h1>;
+        }
+
+        return content;
+    }
+
+    return getPageContent();
 }
 
 export default SavedArticles;
