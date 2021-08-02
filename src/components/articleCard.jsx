89c32fcb77e1bd98b712694b7f64e03f74919f67
MarkyMan4 <markus.hutnik@gmail.com>
2021-08-01 19:57:42 -0500

    show bookmark button on article cards

--- src/components/articleCard.jsx
+++ src/components/articleCard.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import BookmarkButton from './bookmarkButton';
+import { isUserAuthenticated } from '../utils/storage';
 
 function ArticleCard(props) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // useEffect for handling whether the user is logged in
+    useEffect(() => {
+        isUserAuthenticated()
+            .then(res => setIsLoggedIn(res))
+            .catch(err => console.log(err));
+    }, [localStorage.getItem('token')]);
+
+    // style for placing the Bookmark button in the bottom right corner of the card
+    const bottomRightAlign = {
+        position: "absolute",
+        bottom: 0,
+        right: 0
+    }
+
     return (
         <div className="article-card shadow animate__animated animate__fadeInUp">
             <Link to={"/article/" + props.article.id}><h4>{props.article.headline}</h4></Link>
@@ -15,6 +33,15 @@ function ArticleCard(props) {
                 </div>
                 <div className="col-md-6 text-right">
                     {props.article.nlp.topic_name}
+
+                    {
+                        /* only show bookmark button if user is logged in */
+                        isLoggedIn ? 
+                            <div style={bottomRightAlign} className="col-md-6">
+                                <BookmarkButton articleId={props.article.id} />
+                            </div>
+                        : <div></div>
+                    }
                 </div>
             </div>
         </div>
