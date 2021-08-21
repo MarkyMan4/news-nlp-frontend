0ac209a5222783e34a4463d8d727a969fe5c7859
MarkyMan4 <markus.hutnik@gmail.com>
2021-08-21 12:36:37 -0500

    made the bar chart for the visualizations page

--- src/components/barChart.jsx
+++ src/components/barChart.jsx
@@ -2,27 +2,102 @@ import React, { useEffect } from 'react';
 import * as d3 from 'd3';
 import { scaleLinear, scaleBand } from 'd3-scale';
 
-const dummyData = [
-    {x: 1, y: 'test1'},
-    {x: 2, y: 'test2'},
-    {x: 3, y: 'test3'},
-    {x: 4, y: 'test4'}
-]
+const containerWidth = 700;
+const containerHeight = 400;
 
-function BarChart({data, svgRef}) {
+function BarChart({chartData, svgRef, chartTitle}) {
     // handle d3 usage here
     useEffect(() => {
+        // find the max y value for using in the yScale
+        let maxY = Number.MIN_SAFE_INTEGER;
+
+        for(let i = 0; i < chartData.length; i++) {
+            if(chartData[i].y > maxY)
+                maxY = chartData[i].y;
+        }
+
+        const xScale = scaleBand()
+            .domain(chartData.map(data => data.x))
+            .rangeRound([0, containerWidth])
+            .padding(0.25);
+
+        const yScale = scaleLinear()
+            .domain([0, maxY + 250]) // give a little padding at the top of the chart
+            .range([containerHeight, 0]);
+
+        // container (i.e. the svg) that hold the chart
         const container = d3.select(svgRef.current).classed('graph-container', true);
 
+        const handleMouseOver = (d, i) => {
+            d3.select(d.target)
+                .transition()
+                .duration(100)
+                .attr('opacity', 0.75);
+        }
+
+        const handleMouseOut = (d, i) => {
+            d3.select(d.target)
+                .transition()
+                .duration(100)
+                .attr('opacity', 1);
+        }
+
+        const barBottom = containerHeight - 30;
+
+        // create the bar chart
         container
-            .selectAll('circle')
-            .data(dummyData)
+            .selectAll('rect')
+            .data(chartData)
             .enter()
-            .append('circle')
-            .attr('cx', 50)
-            .attr('cy', 50)
-            .attr('r', 10);
-    }, [data, svgRef]);
+            .append('rect')
+            .attr('x', data => xScale(data.x))
+            .attr('y', data => yScale(data.y))
+            .attr('width', xScale.bandwidth())
+            .attr('height', data => barBottom - yScale(data.y))
+            .attr('fill', 'DodgerBlue')
+            .on('mouseover', handleMouseOver)
+            .on('mouseout', handleMouseOut);
+
+        // add labels to each bar to show the data value
+        container
+            .selectAll('.chart-data-label')
+            .data(chartData)
+            .enter()
+            .append('text')
+            .classed('chart-data-label', true)
+            .attr('x', data => xScale(data.x) + (xScale.bandwidth() / 2)) // centered in bars
+            .attr('y', data => yScale(data.y) + 30) // place text near the top of the bar
+            .text(data => data.y);
+
+        // add a line at the base of the bars to separate from the labels
+        container
+            .append('line')
+            .attr('x1', 0)
+            .attr('y1', barBottom)
+            .attr('x2', containerWidth)
+            .attr('y2', barBottom)
+            .attr('stroke', 'black');
+
+        // add text labels to tell what topic each bar represents
+        container
+            .selectAll('.chart-label')
+            .data(chartData)
+            .enter()
+            .append('text')
+            .classed('bar-chart-label', true)
+            .attr('text-anchor', 'middle')
+            .attr('x', data => xScale(data.x) + (xScale.bandwidth() / 2))
+            .attr('y', containerHeight - 10)
+            .text(data => data.x);
+
+        // add the chart title
+        container
+            .append('text')
+            .classed('chart-title', true)
+            .attr('x', containerWidth / 2)
+            .attr('y', 25)
+            .text(chartTitle);
+    }, [chartData, svgRef]);
 
     // no need to return anything since this only manipulates an svg in the parent component
     return null;
--- src/pages/visuals.jsx
+++ src/pages/visuals.jsx
@@ -1,23 +1,28 @@
 import React, { useState, useEffect, useRef } from 'react';
 import BarChart from '../components/barChart';
-// import { getArticleCounts } from '../api/newsRequests';
+import { getArticleCountsByTopic } from '../api/newsRequests';
 
 function Visuals() {
     const barChartRef = useRef(null);
     const [articleCounts, setArticleCounts] = useState([]);
 
-    // need to wait to do this until backend has an endpoint to retrieve article counts by topic
+    useEffect(() => {
+        // retrieve article count for each topic
+        getArticleCountsByTopic()
+            .then(res => {
+                // format data in a nice way for the bar chart (list of data)
+                // [{<topic1>: <count1>}, {<topic2>: <count2>}, ...]
+                const chartData = Object.keys(res).map(topic => {return {x: topic, y: res[topic]}});
 
-    // useEffect(() => {
-    //     getArticleCounts()
-    //         .then(res => console.log(res))
-    //         .catch(err => console.log(err));
-    // }, []);
+                setArticleCounts(chartData);
+            })
+            .catch(err => console.log(err));
+    }, []);
 
     return (
         <div className="ml-5 mr-5">
             <svg ref={barChartRef}></svg>
-            <BarChart svgRef={barChartRef} />
+            <BarChart chartData={articleCounts} svgRef={barChartRef} chartTitle="Article Counts by Topic" />
         </div>
     )
 }
