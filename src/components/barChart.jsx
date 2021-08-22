f42ed63ac29aad30157d28e14d1e0c3d02e91013
MarkyMan4 <markus.hutnik@gmail.com>
2021-08-22 11:15:29 -0500

    added proper axis labels

--- src/components/barChart.jsx
+++ src/components/barChart.jsx
@@ -5,6 +5,12 @@ import { scaleLinear, scaleBand } from 'd3-scale';
 const containerWidth = 700;
 const containerHeight = 400;
 
+// margins for the graph area
+let margin = {
+    bottom: 30,
+    top: 50
+}
+
 function BarChart({chartData, svgRef, chartTitle}) {
     // handle d3 usage here
     useEffect(() => {
@@ -16,17 +22,23 @@ function BarChart({chartData, svgRef, chartTitle}) {
                 maxY = chartData[i].y;
         }
 
+        // lowest bars will show in svg, this allows room for labels on the x-axis
+        const barBottom = containerHeight - margin.bottom;
+
         const xScale = scaleBand()
             .domain(chartData.map(data => data.x))
             .rangeRound([0, containerWidth])
             .padding(0.25);
 
         const yScale = scaleLinear()
-            .domain([0, maxY + 250]) // give a little padding at the top of the chart
-            .range([containerHeight, 0]);
+            .domain([0, maxY + 50]) // give a little padding at the top of the chart
+            .range([barBottom, margin.top]); // need set barBottom as the max so there are bounds for the graph, margin top allows room for title
 
         // container (i.e. the svg) that hold the chart
-        const container = d3.select(svgRef.current).classed('graph-container', true);
+        // this also clears things out between renders so it doesn't get drawn multiple times
+        const container = d3.select(svgRef.current);
+        container.selectAll('*').remove();
+        container.classed('graph-container', true);
 
         const handleMouseOver = (d, i) => {
             d3.select(d.target)
@@ -41,8 +53,6 @@ function BarChart({chartData, svgRef, chartTitle}) {
                 .duration(100)
                 .attr('opacity', 1);
         }
-
-        const barBottom = containerHeight - 30;
 
         // create the bar chart
         container
@@ -69,26 +79,22 @@ function BarChart({chartData, svgRef, chartTitle}) {
             .attr('y', data => yScale(data.y) + 30) // place text near the top of the bar
             .text(data => data.y);
 
-        // add a line at the base of the bars to separate from the labels
-        container
-            .append('line')
-            .attr('x1', 0)
-            .attr('y1', barBottom)
-            .attr('x2', containerWidth)
-            .attr('y2', barBottom)
-            .attr('stroke', 'black');
+        // container.remove('g');
 
         // add text labels to tell what topic each bar represents
+        // TODO: see if there is a way to adjust font size
         container
-            .selectAll('.chart-label')
-            .data(chartData)
-            .enter()
-            .append('text')
-            .classed('bar-chart-label', true)
-            .attr('text-anchor', 'middle')
-            .attr('x', data => xScale(data.x) + (xScale.bandwidth() / 2))
-            .attr('y', containerHeight - 10)
-            .text(data => data.x);
+            .append("g")
+            .attr('transform', `translate(0,${barBottom})`) // This controls the vertical position of the Axis
+            .call(d3.axisBottom(xScale))
+            .selectAll('text')
+                .attr('font-size', '18px');
+
+        // add ticks for the y-axis
+        container
+            .append('g')
+            .attr('transform', `translate(${containerWidth - 35}, 0)`) // This controls the vertical position of the Axis
+            .call(d3.axisRight(yScale));
 
         // add the chart title
         container
@@ -97,6 +103,8 @@ function BarChart({chartData, svgRef, chartTitle}) {
             .attr('x', containerWidth / 2)
             .attr('y', 25)
             .text(chartTitle);
+
+        
     }, [chartData, svgRef]);
 
     // no need to return anything since this only manipulates an svg in the parent component
